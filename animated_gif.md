317e2649351ed54d1da9cbbf3f8eff7c86b83859
Bruce Ravel <bravel@bnl.gov>
2014-07-07 17:10:41 -0400

    more work on instruction images

--- animated_gif.md
+++
@@ -1,6 +0,0 @@
-Demonstration
-=============
-
-This test weather animated gifs work on github.
-
-![This is an animated gif](assembly/2jYFZmY.gif)
--- assembly/copy-n-trim.sh
+++ assembly/copy-n-trim.sh
@@ -5,9 +5,15 @@ dropbox=/home/bruce/Dropbox/Pop-up/AssemblyFigures
 
 cd $dropbox
 files=`ls *.png`
+gifs=`ls *.gif`
 
 cd $here
 for f in $files; do
     echo $f
     convert -trim -bordercolor white -border 5x5 $dropbox/$f $here/$f
+done
+
+for f in $gifs; do
+    echo $f
+    cp $dropbox/$f $here/$f
 done
